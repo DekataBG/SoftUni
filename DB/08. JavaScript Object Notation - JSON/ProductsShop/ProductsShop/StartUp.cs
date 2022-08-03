fa340eb6ad96a9bf2e66d5d68da7360d3e1ef304
DekataBG <dekatawee@abv.bg>
2022-08-03 17:30:04 +0300

    json excercises database created

---
+++ DB/08. JavaScript Object Notation - JSON/ProductsShop/ProductsShop/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using ProductsShop.Data;
+using System;
+
+namespace ProductsShop
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+
+        }
+    }
+}
