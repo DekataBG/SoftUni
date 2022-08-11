2e3271855f747b5b723a272ce31ca5d2e9e5b4c5
DekataBG <dekatawee@abv.bg>
2022-08-11 22:14:47 +0300

    problem 3 fixed

--- DB/10. Exams/Footballers/DataProcessor/Serializer.cs
+++ DB/10. Exams/Footballers/DataProcessor/Serializer.cs
@@ -14,8 +14,9 @@
     {
         public static string ExportCoachesWithTheirFootballers(FootballersContext context)
         {
-            var coaches = context.Coaches.ToArray()
-                .Where(c => c.Footballers.Count > 0)
+            var coaches = context.Coaches
+                .Where(c => c.Footballers.Any())
+                .ToArray()
                 .Select(c => new CoachExportDto
                 {
                     FootballersCount = c.Footballers.Count(),
@@ -24,10 +25,12 @@
                     {
                         Name = f.Name,
                         Position = f.PositionType.ToString()
-                    }).OrderBy(f => f.Name)
+                    })
+                    .OrderBy(f => f.Name)
                     .ToArray(),
                 })
                 .OrderByDescending(c => c.FootballersCount)
+                .ThenBy(c => c.CoachName)
                 .ToArray();
 
 
@@ -38,34 +41,31 @@
 
         public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
         {
-            //31/03/2020
-
             var teams = context.Teams
-                //.Where(t => t.TeamsFootballers
-                    //.Select(tf => tf.Footballer)
-                    //.Any(f => f.ContractStartDate >= date))
+                .Where(t => t.TeamsFootballers
+                    .Any(tf => tf.Footballer.ContractStartDate >= date))
+                .ToArray()
                 .Select(t => new
                 {
                     Name = t.Name,
                     Footballers = t.TeamsFootballers
-                        .Select(tf => tf.Footballer)
-                        //.Where(f => f.ContractStartDate >= date)
+                        .Where(tf => tf.Footballer.ContractStartDate >= date)
+                         .OrderByDescending(x => x.Footballer.ContractEndDate)
+                        .ThenBy(x => x.Footballer.Name)
                     .Select(f => new
                     {
-                        FootballerName = f.Name,
-                        ContractStartDate = f.ContractStartDate,
-                        //ContractEndDate = DateTime.ParseExact(f.ContractEndDate.ToString(), "mm/dd/yyyy", CultureInfo.InvariantCulture),
-                        ContractEndDate = f.ContractEndDate.ToString(),
-                        BestSkillType = f.BestSkillType,
-                        PositionType = f.PositionType
+                        FootballerName = f.Footballer.Name,
+                        ContractStartDate = f.Footballer.ContractStartDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                        ContractEndDate = f.Footballer.ContractEndDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                        BestSkillType = f.Footballer.BestSkillType.ToString(),
+                        PositionType = f.Footballer.PositionType.ToString()
                     })
-                    .OrderByDescending(f => f.ContractEndDate)
-                    .ThenBy(f => f.FootballerName)
-                    .ToList()
-                }).ToList()
-                .OrderByDescending(t => t.Footballers.Count)
+                    .ToArray()
+                })
+                .OrderByDescending(t => t.Footballers.Count())
                 .ThenBy(t => t.Name)
-                .ToList().Take(5);
+                .Take(5)
+                .ToArray();
 
             return JsonConvert.SerializeObject(teams, Formatting.Indented);
         }
