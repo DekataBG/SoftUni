9d51dbb242c72f2329710c64bb05284f0d4e7466
DekataBG <dekatawee@abv.bg>
2022-08-23 18:30:02 +0300

    binary search tree and max heap problems solved

--- Data Structures/03. Heaps and BST - Lab/02.BinarySearchTree/BinarySearchTree.cs
+++ Data Structures/03. Heaps and BST - Lab/02.BinarySearchTree/BinarySearchTree.cs
@@ -1,30 +1,156 @@
 ﻿namespace _02.BinarySearchTree
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Xml.Linq;
 
     public class BinarySearchTree<T> : IBinarySearchTree<T>
         where T : IComparable<T>
     {
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; set; }
+
+            public Node Left { get; set; }
+
+            public Node Right { get; set; }
+        }
+
+        private Node root;
+
         public BinarySearchTree() { }
+
+        private BinarySearchTree(Node node)
+        {
+            CopyPreOrder(node);
+        }
 
         public bool Contains(T element)
         {
-            throw new NotImplementedException();
+            var nodes = new List<T>();
+
+            EachInOrder(n => nodes.Add(n));
+
+            foreach (var node in nodes)
+            {
+                if (node.Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void EachInOrder(Action<T> action)
         {
-            throw new NotImplementedException();
+            EachInOrder(action, root);
         }
 
         public void Insert(T element)
         {
-            throw new NotImplementedException();
+            if (root is null)
+            {
+                root = new Node(element);
+                return;
+            }
+
+            Insert(element, root);
         }
 
         public IBinarySearchTree<T> Search(T element)
         {
-            throw new NotImplementedException();
+            var node = FindNode(element);
+
+            if (node is null)
+            {
+                return null;
+            }
+
+            return new BinarySearchTree<T>(node);
+        }
+
+        private void EachInOrder(Action<T> action, Node node)
+        {
+            if (node is null)
+            {
+                return;
+            }
+
+            EachInOrder(action, node.Left);
+
+            action(node.Value);
+
+            EachInOrder(action, node.Right);
+        }
+
+        private void Insert(T element, Node node)
+        {
+            if (element.CompareTo(node.Value) > 0)
+            {
+                if (node.Right is null)
+                {
+                    node.Right = new Node(element);
+                }
+                else
+                {
+                    Insert(element, node.Right);
+                }
+            }
+            else if (element.CompareTo(node.Value) < 0)
+            {
+                if (node.Left is null)
+                {
+                    node.Left = new Node(element);
+                }
+                else
+                {
+                    Insert(element, node.Left);
+                }
+            }
+        }
+
+        private IEnumerable<Node> GetNodesPreOrder(ICollection<Node> result, Node node)
+        {
+            if (node is null)
+            {
+                return result;
+            }
+
+            result.Add(node);
+
+            GetNodesPreOrder(result, node.Left);
+
+            GetNodesPreOrder(result, node.Right);
+
+            return result;
+        }
+
+        private Node FindNode(T element)
+        {
+            var node = GetNodesPreOrder(new Collection<Node>(), root)
+                .FirstOrDefault(n => n.Value.Equals(element));
+            return node;
+        }
+
+        private void CopyPreOrder(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Insert(node.Value);
+            CopyPreOrder(node.Left);
+            CopyPreOrder(node.Right);
         }
     }
 }
--- Data Structures/03. Heaps and BST - Lab/03.MaxHeap/MaxHeap.cs
+++ Data Structures/03. Heaps and BST - Lab/03.MaxHeap/MaxHeap.cs
@@ -2,24 +2,78 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Drawing;
 
     public class MaxHeap<T> : IAbstractHeap<T> where T : IComparable<T>
     {
-        public int Size => throw new NotImplementedException();
+        private List<T> elements;
+
+        public MaxHeap()
+        {
+            elements = new List<T>();
+        }
+
+        public int Size => elements.Count;
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            elements.Add(element);
+
+            Heapify(Size - 1);
         }
 
         public T ExtractMax()
         {
-            throw new NotImplementedException();
+            if (Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var maxElement = elements[0];
+
+            elements.RemoveAt(0);
+
+            return maxElement;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return elements[0];
+        }
+
+        private void Heapify(int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            var parentIndex = GetParentIndex(index);
+
+            if (elements[parentIndex].CompareTo(elements[index]) < 0)
+            {
+                SwapElements(index, parentIndex);
+                Heapify(parentIndex);
+            }
+        }
+
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        private void SwapElements(int index1, int index2)
+        {
+            var element1 = elements[index1];
+            var element2 = elements[index2];
+
+            elements[index1] = element2;
+            elements[index2] = element1;
         }
     }
 }
