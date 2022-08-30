d15ea31967e85e36671d046844ff36e15e42cb0c
DekataBG <dekatawee@abv.bg>
2022-08-30 07:17:36 +0300

    browser history problems solved

---
+++ Data Structures/01. BrowserHistory/BrowserHistory.cs
@@ -0,0 +1,161 @@
+﻿namespace _01._BrowserHistory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using _01._BrowserHistory.Interfaces;
+
+    public class BrowserHistory : IHistory
+    {
+        LinkedList<ILink> links;
+
+        public BrowserHistory()
+        {
+            links = new LinkedList<ILink>();
+        }
+
+        public int Size => links.Count;
+
+        public void Clear()
+        {
+            links.Clear();
+        }
+
+        public bool Contains(ILink link)
+        {
+            return links.Contains(link);
+        }
+
+        public ILink DeleteFirst()
+        {
+            EnsureNotEmpty();
+
+            ILink link = links.Last.Value;
+
+            links.RemoveLast();
+
+            return link;
+        }
+
+        public ILink DeleteLast()
+        {
+            EnsureNotEmpty();
+
+            var last = links.First.Value;
+
+            links.RemoveFirst();
+
+            return last;
+        }
+
+        public ILink GetByUrl(string url)
+        {
+            var link = links.FirstOrDefault(l => l.Url == url);
+
+            return link;
+        }
+
+        public ILink LastVisited()
+        {
+            EnsureNotEmpty();
+
+            var link = links.First.Value;
+
+            return link;
+        }
+
+        public void Open(ILink link)
+        {
+            links.AddFirst(link);
+        }
+
+        public int RemoveLinks(string url)
+        {
+            EnsureNotEmpty();
+
+            url = url.ToLower();
+
+            int count = 0;
+
+            var currentNode = links.First;
+
+            while (currentNode != null)
+            {
+                var nextNode = currentNode.Next;
+
+                if (currentNode.Value.Url.ToLower().Contains(url))
+                {
+                    links.Remove(currentNode.Value);
+                    count++;
+                }
+
+                currentNode = nextNode;
+            }
+
+            if (count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return count;
+            
+
+            //var linksWithUrl = links
+            //    .Where(l => l.Url.ToLower().Contains(url.ToLower()));
+
+            //if (linksWithUrl.Count() ==  0)
+            //{
+            //    throw new InvalidOperationException();
+            //}
+
+            //var linksToRemove = new List<ILink>();
+
+            //foreach (var link in linksWithUrl)
+            //{
+            //    linksToRemove.Add(link);
+            //}
+
+
+            //foreach (var link in linksToRemove)
+            //{
+            //    links.Remove(link);
+            //}
+
+            //return linksToRemove.Count;
+        }
+
+        public ILink[] ToArray()
+        {
+            return links.ToArray();
+        }
+
+        public List<ILink> ToList()
+        {
+            return links.ToList();
+        }
+
+        public string ViewHistory()
+        {
+            if (Size == 0)
+            {
+                return "Browser history is empty!";
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            ToList().ForEach(l => stringBuilder.AppendLine(l.ToString()));
+
+            return stringBuilder.ToString();
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ Data Structures/04. Data Structures Fundamentals with C# Retake Exam - 8 August 2021/Hearthstone/Board.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+
+public class Board : IBoard
+{
+    private Dictionary<string, Card> cards;
+
+    public Board()
+    {
+        cards = new Dictionary<string, Card>();
+    }
+
+    public bool Contains(string name)
+    {
+        return cards.ContainsKey(name);
+    }
+
+    public int Count()
+    {
+        return cards.Count;
+    }
+
+    public void Draw(Card card)
+    {
+        if (Contains(card.Name))
+        {
+            throw new ArgumentException();
+        }
+
+        cards[card.Name] = card;
+    }
+
+    public IEnumerable<Card> GetBestInRange(int start, int end)
+    {
+        var cardsInRange = cards
+            .Where(c => c.Value.Score >= start && c.Value.Score <= end)
+            .Select(c => c.Value)
+            .OrderByDescending(c => c.Level);
+
+        return cardsInRange;
+    }
+
+    public void Heal(int health)
+    {
+        var carWithLowestHeal = cards
+            .Select(c => new
+            {
+                Name = c.Value.Name,
+                Health = c.Value.Health
+            })
+            .OrderBy(c => c.Health)
+            .ToArray()[0];
+
+        var oldCard = cards[carWithLowestHeal.Name];
+        oldCard.Health += health;
+
+        cards[carWithLowestHeal.Name] = oldCard;
+    }
+
+    //TODO
+    public IEnumerable<Card> ListCardsByPrefix(string prefix)
+    {
+        var cardsWithPrefix = cards
+            .Where(c => c.Value.Name.StartsWith(prefix))
+            .Select(c => c.Value)
+            .OrderBy(c => c.Name[c.Name.Length - 1])
+            .ThenBy(c => c.Level);
+            ;
+
+        return cardsWithPrefix;
+    }
+
+    public void Play(string attackerCardName, string attackedCardName)
+    {
+        Card attacker, attacked;
+
+        try
+        {
+            attacker = cards[attackerCardName];
+            attacked = cards[attackedCardName];
+        }
+        catch (Exception)
+        {
+            throw new ArgumentException();
+        }
+
+        if (attacker.Level != attacked.Level)
+        {
+            throw new ArgumentException();
+        }
+
+        if (attacked.Health > 0)
+        {
+            attacked.Health -= attacker.Damage;
+
+            if (attacked.Health <= 0)
+            {
+                attacker.Score += attacked.Level;
+            }
+        }
+    }
+
+    public void Remove(string name)
+    {
+        if (cards.Remove(name) == false)
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    public void RemoveDeath()
+    {
+        var list = new List<string>();
+
+        cards
+        .Where(c => c.Value.Health <= 0)
+        .Select(c => c.Value.Name)
+        .ToList()
+        .ForEach(c => list.Add(c));
+
+        foreach (var card in list)
+        {
+            Remove(card);
+        }
+    }
+
+    //TODO
+    public IEnumerable<Card> SearchByLevel(int level)
+    {
+        var cardsWithLevel = cards
+            .Where(c => c.Value.Level == level)
+            .Select(c => c.Value)
+            .OrderByDescending(c => c.Score);
+
+        return cardsWithLevel;
+    }
+}
