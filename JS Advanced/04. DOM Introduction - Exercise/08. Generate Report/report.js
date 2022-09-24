ed5965ed1a5d3003216ef093cb63476645698adc
DekataBG <dekatawee@abv.bg>
2022-09-24 17:33:01 +0300

    DOM Introduction - Exercise - Problems Solved

--- JS Advanced/04. DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS Advanced/04. DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,36 @@
 function generateReport() {
-    //TODO
+    let indexes = {};
+
+    Array.from(document.querySelectorAll('input[type="checkbox"]'))
+        .forEach((c, i) => {
+            if(c.checked){
+                indexes[i] = c.name;
+            }
+        });
+
+    let keys = Object.keys(indexes)
+        .map(i => Number(i));
+
+    let employeeInfo = Array.from(document.querySelectorAll('tbody tr'))
+        .map(e => Array.from(e.children)
+            .filter((x, i) => {
+                if(keys.includes(i)){
+                    return x;
+                }
+            })
+            .map((x, i) => { 
+                let obj = {};
+                let field = keys[i];
+                obj[indexes[field]] = x.textContent;    
+
+                return obj;
+            }))
+        .map(x => {
+            let emp = {};
+            x.forEach(y => emp[Object.keys(y)[0]] = Object.values(y)[0]);
+
+            return emp;
+        });
+
+    document.getElementById('output').value = JSON.stringify(employeeInfo);
 }
--- JS Advanced/04. DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ JS Advanced/04. DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,85 @@
 function solve() {
+    addOptions();
 
-    //TODO...
+    document.querySelector('button').addEventListener('click', onClick);
+
+    function onClick(){ 
+        let numericalSystem = document.getElementById('selectMenuTo').value;
+        let number = Number(document.getElementById('input').value);
+
+        let result;
+
+        if(numericalSystem == 'binary') {
+            result = convertToBinary(number);
+        } else if(numericalSystem == 'hexadecimal') {
+            result = convertToHexadecimal(number);
+        }
+
+        document.getElementById('result').value = result;
+
+        
+        function convertToBinary(number){
+            let result = [];
+
+            while(number != 0) {
+                let previousNumber = number;
+                number = Math.floor(number / 2);
+
+                let remainder = previousNumber - number * 2;
+                result.unshift(remainder);
+            }
+
+            return result.join('');
+        }
+
+        function convertToHexadecimal(number){
+            let result = [];
+            
+            while(number != 0) {
+                let previousNumber = number;
+                number = Math.floor(number / 16);
+
+                let remainder = previousNumber - number * 16;
+                switch(remainder) {
+                    case 10: 
+                        remainder = 'A';
+                        break;
+                    case 11: 
+                        remainder = 'B';
+                        break;
+                    case 12: 
+                        remainder = 'C';
+                        break;
+                    case 13: 
+                        remainder = 'D';
+                        break;
+                    case 14: 
+                        remainder = 'E';
+                        break;
+                    case 15: 
+                        remainder = 'F'; 
+                        break;
+                }
+
+                result.unshift(remainder);
+            }
+
+            return result.join('');
+        }
+    }
+
+    function addOptions(){
+        let select = document.getElementById('selectMenuTo');
+
+        var option1 = document.createElement('option');
+        option1.value = "binary";
+        option1.innerHTML = "Binary";
+        
+        var option2 = document.createElement('option');
+        option2.value = "hexadecimal";
+        option2.innerHTML = "Hexadecimal";
+    
+        select.appendChild(option1);
+        select.appendChild(option2);    
+    }
 }
