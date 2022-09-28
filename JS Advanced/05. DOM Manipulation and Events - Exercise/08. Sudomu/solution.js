24c4cc969c4bd225f8832ffda17e3baa66ce206f
DekataBG <dekatawee@abv.bg>
2022-09-28 14:16:31 +0300

    DOM Manipulation and EventsExercise-ProblemsSolved

--- JS Advanced/05. DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
+++ JS Advanced/05. DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
@@ -1,4 +1,67 @@
 function solve() {
+    let [checkButton, clearButton] = document.querySelectorAll("button");
+    checkButton.addEventListener("click", onClick);
+    clearButton.addEventListener("click", clear);
 
-    //TODO...
+    function onClick() {
+        let table = document.querySelector("table");
+        let output = document.getElementById("check").children[0];
+        let isSolved = solved();
+
+        table.style.border = isSolved ? "2px solid green" : "2px solid red";
+        output.textContent = isSolved ? "You solve it! Congratulations!" : "NOP! You are not done yet...";
+        output.style.color = isSolved ? "green" : "red";
+        
+
+        function solved() {
+            if(Array.from(document.querySelectorAll("input[type='number']"))
+                .some(n => n.value == "")){
+                    return false;
+            }
+
+            let numbers = Array.from(document.querySelectorAll("input[type='number']"))
+                .map(n => Number(n.value));
+
+            let matrix = [[numbers[0], numbers[1], numbers[2]], 
+            [numbers[3], numbers[4], numbers[5]], 
+            [numbers[6], numbers[7], numbers[8]]];
+
+            let sum = matrix[0][0] + matrix[0][1] + matrix[0][2];
+
+            for(let i = 0; i < 3; i++) {
+                let currentSum = 0; 
+                for(let j = 0; j < 3; j++) {
+                    currentSum += matrix[i][j];
+                }
+
+                if(currentSum != sum) {
+                    return false;
+                }
+            }
+
+            for(let i = 0; i < 3; i++) {
+                let currentSum = 0; 
+                for(let j = 0; j < 3; j++) {
+                    currentSum += matrix[j][i];
+                }
+
+                if(currentSum != sum) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    function clear() {
+        let table = document.querySelector("table");        
+        let output = document.getElementById("check").children[0];
+
+        table.style.border = "";
+        output.textContent = "";
+
+        Array.from(document.querySelectorAll("input[type='number']"))
+            .forEach(n => n.value = "");
+    }
 }
--- JS Advanced/05. DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
+++ JS Advanced/05. DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-  //TODO...
+  let right = 0;
+  let wrong = 0;
+
+  let paragraphs = Array.from(document.querySelectorAll("p"));
+  paragraphs.forEach(p => p.addEventListener("click", goToNextPage));
+  let sections = document.querySelectorAll("section");
+  let result = document.querySelector("#results .results-inner h1");
+
+  function goToNextPage() {
+    if(this.textContent == "onclick" || this.textContent == "JSON.stringify()" || this.textContent == "A programming API for HTML and XML documents") {
+      right++;
+    } else {
+      wrong++;
+    }
+
+    sections[right + wrong - 1].style.display = "none";
+
+    if(right + wrong < 3){
+      sections[right + wrong].style.display = "block";          
+    } else {
+      document.getElementById("results").style.display = "block";
+
+      if(right == 3){
+        result.textContent = "You are recognized as top JavaScript fan!";
+      } else {
+        result.textContent = `You have ${right} right answers`;
+      }
+    }
+  }
 }
